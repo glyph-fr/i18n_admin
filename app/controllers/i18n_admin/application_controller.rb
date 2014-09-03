53925b8b2e7bbad44a89aaa3c47c1f2a54049655
vala <gonjo@free.fr>
2014-09-03 11:46:32 +0200

    fix authentication and drop menu bar to easily include in iframe

--- app/controllers/i18n_admin/application_controller.rb
+++ app/controllers/i18n_admin/application_controller.rb
@@ -1,13 +1,13 @@
 module I18nAdmin
   class ApplicationController < ::ApplicationController
-    # if (authentication_method = I18nAdmin.authentication_method)
-    #   send(authentication_method)
-    # end
+    if (authentication_method = I18nAdmin.authentication_method)
+      before_filter authentication_method
+    end
 
-    # def current_user
-    #   if (current_user_method = I18nAdmin.current_user_method)
-    #     send(current_user_method)
-    #   end
-    # end
+    def current_user
+      if (current_user_method = I18nAdmin.current_user_method)
+        send(current_user_method)
+      end
+    end
   end
 end
