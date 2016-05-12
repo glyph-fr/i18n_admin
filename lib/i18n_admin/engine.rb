cc92bbcb89382a75299fc42212fef520c4635072
vala <gonjo@free.fr>
2016-05-12 11:22:06 +0200

    precompile spinner.gif in engine

--- lib/i18n_admin/engine.rb
+++ lib/i18n_admin/engine.rb
@@ -2,7 +2,7 @@ module I18nAdmin
   class Engine < ::Rails::Engine
     isolate_namespace I18nAdmin
 
-    initializer 'Configure I18n backend' do
+    initializer 'i18n_admin.configure_i18n_backend' do
       translations_installed = begin
         I18nAdmin::TranslationsSet.select('1').inspect
         true
@@ -17,6 +17,12 @@ module I18nAdmin
           I18n.backend
         )
       end
+    end
+
+    initializer 'i18n_admin.add_assets_to_precompilation', group: :all do |app|
+      app.config.assets.precompile += %w(
+        'i18n_admin/spinner.gif'
+      )
     end
   end
 end
